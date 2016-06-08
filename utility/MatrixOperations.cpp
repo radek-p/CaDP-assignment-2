743b274725e2de9b639773e44bd97358c64a2f23
Radosław Piórkowski <rp335451@students.mimuw.edu.pl>
2016-06-08 18:07:00 +0200

    Use SIMD instructions for daxpy operation

--- utility/MatrixOperations.cpp
+++ utility/MatrixOperations.cpp
@@ -5,30 +5,43 @@
 #include "MatrixOperations.h"
 #include <emmintrin.h>
 
-//struct saxpy_kernel
-//{
-//public:
-//    enum { BLOCK_SIZE = 8 }; // note that instead of 4, we now process 8 elements
-//    saxpy_kernel(double a) : a(a) {}
-//    inline void operator()(double* y, const double* x) const {
-//        (*y) += a * (*x);
-//    }
-//    inline void block(double* y, const double* x) const {
-//        // load 8 data elements at a time
-//        __m128d X1 = _mm_loadu_pd(x + 0); // _mm_loadu_ps(x + 0);
-//        __m128d X2 = _mm_loadu_pd(x + 2);
-//        __m128d Y1 = _mm_loadu_pd(y + 0);
-//        __m128d Y2 = _mm_loadu_pd(y + 2);
-//        // do the computations
-//        __m128 result0 = _mm_add_ps(Y1, _mm_mul_ps(X1, _mm_set1_ps(a)));
-//        __m128 result1 = _mm_add_ps(Y2, _mm_mul_ps(X2, _mm_set1_ps(a)));
-//        // store the results
-//        _mm_storeu_ps(y + 0, result1);
-//        _mm_storeu_ps(y + 4, result2);
-//    }
-//protected:
-//    double a;
-//};
+#define ROUND_DOWN(x, s) ((x) & ~((s)-1))
+struct multiplicationKernel {
+public:
+    enum { BLOCK_SIZE = 4 }; // note that instead of 2, we now process 4 elements
+    multiplicationKernel(double a) : multiplier(a) {}
+    inline void operator()(double* y, const double* x) const {
+        (*y) += multiplier * (*x);
+    }
+    inline void block(double* y, const double* x) const {
+        // load 4 data elements at a time
+        __m128d X1 = _mm_loadu_pd(x + 0);
+        __m128d X2 = _mm_loadu_pd(x + 2);
+        __m128d Y1 = _mm_loadu_pd(y + 0);
+        __m128d Y2 = _mm_loadu_pd(y + 2);
+
+        // do the computations
+        __m128d result0 = _mm_add_pd(Y1, _mm_mul_pd(X1, _mm_set1_pd(multiplier)));
+        __m128d result1 = _mm_add_pd(Y2, _mm_mul_pd(X2, _mm_set1_pd(multiplier)));
+
+        // store the results
+        _mm_storeu_pd(y + 0, result0);
+        _mm_storeu_pd(y + 2, result1);
+    }
+public:
+    double multiplier;
+
+};
+
+template<class OutputPointer, class InputPointer, class Operation>
+void executeKernel(OutputPointer dst, InputPointer src, size_t length, Operation op) {
+    size_t n;
+    // execute op on array elements block-wise
+    for(n = 0; n < ROUND_DOWN(length, op.BLOCK_SIZE); n += op.BLOCK_SIZE)
+        op.block(dst + n, src + n);
+    // execute the remaining array elements one by one
+    for(; n < length; n++) op(dst + n, src + n);
+}
 
 void DeferredSparseDenseMultiplication::addTo(DenseMatrixFragment &C) {
     exec(C, false);
@@ -62,24 +75,19 @@ void DeferredSparseDenseMultiplication::exec(DenseMatrixFragment &C, bool overri
     assert(C.size().kRow() >= kRow);
     assert(C.size().kCol() >= kCol);
 
-//    for (int row = pRow; row < kRow; ++row) {
-//        for (int rowElementIdx = A->rowIntervals[row]; rowElementIdx < A->rowIntervals[row+1]; ++rowElementIdx) {
-//            for (int col = pCol; col < kCol; ++col) {
-//                C.at(row, col) += A->entries[rowElementIdx] * B->at(A->columns[rowElementIdx], col);
-//            }
-//        }
-//    }
-
+    multiplicationKernel kernel(0);
 
     for (int row = pRow; row < kRow; ++row) {
         for (int rowElementIdx = A->rowIntervals[row]; rowElementIdx < A->rowIntervals[row+1]; ++rowElementIdx) {
-            const double multiplier = A->entries[rowElementIdx];
+            kernel.multiplier = A->entries[rowElementIdx];
             double *cElem = &C.at(row, pCol);
             double *bElem = &B->at(A->columns[rowElementIdx], pCol);
-            for (int col = pCol; col < kCol; ++col) {
-                *cElem += multiplier * (*bElem);
-                ++cElem; ++bElem;
-            }
+
+            executeKernel(cElem, bElem, (size_t) (kCol - pCol), kernel);
+//            for (int col = pCol; col < kCol; ++col) {
+//                *cElem += multiplier * (*bElem);
+//                ++cElem; ++bElem;
+//            }
         }
     }
 }
